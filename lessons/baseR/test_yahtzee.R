9264b7dacb0d7ca68bcce747b6ce0eff53d79db4
mglover <mglover@bowiestate.edu>
2024-01-17 14:34:54 -0500

    add files

---
+++ lessons/baseR/test_yahtzee.R
@@ -0,0 +1,12 @@
+
+yahtzee <- data.frame(vinny=c(4, 2, 12, 8, 20, 24, 22, 0, 25, 30, 40, 0, 18, 0), mary = c(3, 8, 9, 12, 10, 24, 13, 10, 25, 30, 40, 50, 21, 0))
+
+row.names(yahtzee) <- c('ones', 'twos', 'threes', 'fours', 'fives', 'sixes', 'threeKind', 'fourKind', 'full_house', 'small_straight', 'large_straight', 'yahtzee', 'chance', 'yahtzee_bonus')
+
+
+top <- apply(yahtzee[1:6,], 2, sum)
+top_bonus <- top + 35
+bottom <- apply(yahtzee[7:14,],2,  sum)
+
+max(top + bottom)
+
