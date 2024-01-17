5054fbcbda065c35eb3b9b1b5b02a8e456a16b17
mglover <mglover@bowiestate.edu>
2024-01-17 15:40:08 -0500

    base slides add

--- lessons/baseR/test_yahtzee.R
+++ lessons/baseR/test_yahtzee.R
@@ -1,3 +1,4 @@
+# Testing yahtzee exercise
 
 yahtzee <- data.frame(vinny=c(4, 2, 12, 8, 20, 24, 22, 0, 25, 30, 40, 0, 18, 0), mary = c(3, 8, 9, 12, 10, 24, 13, 10, 25, 30, 40, 50, 21, 0))
 
@@ -9,4 +10,7 @@ top_bonus <- top + 35
 bottom <- apply(yahtzee[7:14,],2,  sum)
 
 max(top + bottom)
+
+total <- top + bottom
+which.max(total)
 
